cade2d6d3d24f252012264ff679af72cfc94d3f0
bear109 <jokerbear109@gmail.com>
2022-11-30 00:49:59 +0900

    feat: 메인화면 검색창 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,27 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Layout from 'components/Layout';
+import WeatherBox from 'components/WeatherBox';
+import { FaSearch } from 'react-icons/fa';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="mx-auto opacity-75 bg-2">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex items-center justify-center w-1/3 border-b-2">
+          <FaSearch color="white" size={25} />
+          <input
+            className="w-full py-2 pr-3 ml-5 text-white bg-transparent placeholder:text-slate-100 focus:outline-none"
+            placeholder="지역을 입력하세요. (ex.강남구, 송파구)"
+            type="text"
+          />
+        </div>
+
+        <div>
+          <WeatherBox />
+          <WeatherBox />
+          <WeatherBox />
+          <WeatherBox />
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Sans KR"'],
+      },
+      backgroundImage: {
+        1: "url('/Images/bg-image-1.jpg')",
+        2: "url('/Images/bg-image-2.jpg')",
+      },
+    },
   },
   plugins: [],
 };
